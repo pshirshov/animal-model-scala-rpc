ea0f75a1383a779db5595a17c011f5f8f6321e76
pshirshov <pshirshov@gmail.com>
2018-04-06 18:39:27 +0100

    cleanups

--- src/main/scala/com/github/pshirshov/izumi/r2/idealingua/experiments/Test.scala
+++ src/main/scala/com/github/pshirshov/izumi/r2/idealingua/experiments/Test.scala
@@ -57,26 +57,26 @@ class SimpleMarshallerImpl(codec: MuxedCodec) extends TransportMarshallers[Strin
   }
 }
 
+class DirectMarshallerImpl() extends TransportMarshallers[String, GreeterServiceWrapped.GreeterServiceInput, GreeterServiceWrapped.GreeterServiceOutput, String] {
+  override def decodeRequest(requestWire: String): GreeterServiceWrapped.GreeterServiceInput = parse(requestWire).flatMap(_.as[GreeterServiceWrapped.GreeterServiceInput]).right.get
+
+  override def encodeRequest(request: GreeterServiceWrapped.GreeterServiceInput): String = request.asJson.noSpaces
+
+  override def decodeResponse(responseWire: String): GreeterServiceWrapped.GreeterServiceOutput = parse(responseWire).flatMap(_.as[GreeterServiceWrapped.GreeterServiceOutput]).right.get
+
+  override def encodeResponse(response: GreeterServiceWrapped.GreeterServiceOutput): String = response.asJson.noSpaces
+
+}
+
+
 object TestMul {
 
   class SingleServiceDemo[R[_] : ServiceResult] {
     println(s"SingleServiceDemo: Running demo with container ${implicitly[ServiceResult[R]]}")
     println()
 
-    val marshalling: TransportMarshallers[String, GreeterServiceWrapped.GreeterServiceInput, GreeterServiceWrapped.GreeterServiceOutput, String] =
-      new TransportMarshallers[String, GreeterServiceWrapped.GreeterServiceInput, GreeterServiceWrapped.GreeterServiceOutput, String] {
-        override def decodeRequest(requestWire: String): GreeterServiceWrapped.GreeterServiceInput = parse(requestWire).flatMap(_.as[GreeterServiceWrapped.GreeterServiceInput]).right.get
-
-        override def encodeRequest(request: GreeterServiceWrapped.GreeterServiceInput): String = request.asJson.noSpaces
-
-        override def decodeResponse(responseWire: String): GreeterServiceWrapped.GreeterServiceOutput = parse(responseWire).flatMap(_.as[GreeterServiceWrapped.GreeterServiceOutput]).right.get
-
-        override def encodeResponse(response: GreeterServiceWrapped.GreeterServiceOutput): String = response.asJson.noSpaces
-      }
-
-
+    val marshalling = new DirectMarshallerImpl()
     val service = new AbstractGreeterServer.Impl[R]
-
     val serverDispatcher = new GreeterServiceWrapped.GreeterServiceDispatcherUnpacking.Impl(service)
 
     val server = new ServerReceiver(serverDispatcher, marshalling)
