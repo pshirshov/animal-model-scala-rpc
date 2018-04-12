c8e83026d72dab7394b444b186c043bcca1b942f
pshirshov <pshirshov@gmail.com>
2018-04-12 21:14:49 +0100

    working contexts example

--- src/main/scala/com/github/pshirshov/izumi/r2/idealingua/experiments/direct_marshallers.scala
+++ src/main/scala/com/github/pshirshov/izumi/r2/idealingua/experiments/direct_marshallers.scala
@@ -5,26 +5,20 @@ import com.github.pshirshov.izumi.r2.idealingua.experiments.runtime.{InContext, 
 
 import scala.language.{higherKinds, implicitConversions}
 
-class DirectMarshallerServerImpl() extends TransportMarshallers[InContext[String, Unit], InContext[GreeterServiceWrapped.GreeterServiceInput, Unit], GreeterServiceWrapped.GreeterServiceOutput, String] {
-
-  import io.circe.parser._
-  import io.circe.syntax._
+class DirectMarshallerServerImpl extends TransportMarshallers[InContext[String, Unit], InContext[GreeterServiceWrapped.GreeterServiceInput, Unit], GreeterServiceWrapped.GreeterServiceOutput, String] {
+  val just = new DirectMarshallerClientImpl()
 
   override def decodeRequest(requestWire: InContext[String, Unit]): InContext[GreeterServiceWrapped.GreeterServiceInput, Unit] = {
-    InContext(parse(requestWire.value).flatMap(_.as[GreeterServiceWrapped.GreeterServiceInput]).right.get, requestWire.context)
+    InContext(just.decodeRequest(requestWire.value), requestWire.context)
   }
 
   override def encodeRequest(request: InContext[GreeterServiceWrapped.GreeterServiceInput, Unit]): InContext[String, Unit] = {
-    InContext(request.value.asJson.noSpaces, request.context)
+    InContext(just.encodeRequest(request.value), request.context)
   }
 
-  override def decodeResponse(responseWire: String): GreeterServiceWrapped.GreeterServiceOutput = {
-    parse(responseWire).flatMap(_.as[GreeterServiceWrapped.GreeterServiceOutput]).right.get
-  }
+  override def encodeResponse(response: GreeterServiceWrapped.GreeterServiceOutput): String = just.encodeResponse(response)
 
-  override def encodeResponse(response: GreeterServiceWrapped.GreeterServiceOutput): String = {
-    response.asJson.noSpaces
-  }
+  override def decodeResponse(responseWire: String): GreeterServiceWrapped.GreeterServiceOutput = just.decodeResponse(responseWire)
 
 }
 
@@ -33,7 +27,7 @@ class DirectMarshallerClientImpl() extends TransportMarshallers[String, GreeterS
   import io.circe.parser._
   import io.circe.syntax._
 
-  override def decodeRequest(requestWire: String): GreeterServiceWrapped.GreeterServiceInput= {
+  override def decodeRequest(requestWire: String): GreeterServiceWrapped.GreeterServiceInput = {
     parse(requestWire).flatMap(_.as[GreeterServiceWrapped.GreeterServiceInput]).right.get
   }
 
